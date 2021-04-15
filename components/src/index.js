a045e249ac3b650ca8c5ff707b0e2e612ebaf8b4
Wouter Bosch <wouterbosch@appstudio.nl>
2021-04-15 10:33:25 +0200

    finish components section

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={faker.name.firstName()}
-        text={faker.lorem.sentence()}
-        timeAgo={faker.date.recent().toString()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        text={faker.lorem.sentence()}
-        timeAgo={faker.date.recent().toString()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        text={faker.lorem.sentence()}
-        timeAgo={faker.date.recent().toString()}
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          text={faker.lorem.sentence()}
+          timeAgo="18/01/2021"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          text={faker.lorem.sentence()}
+          timeAgo="19/01/2021"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          text={faker.lorem.sentence()}
+          timeAgo="17/01/2021"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
