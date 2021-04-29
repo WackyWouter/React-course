8f861251b31ba3f63d8e525b927a8ec06b0a06fe
Wouter Bosch <wfcbosch@gmail.com>
2021-04-29 11:45:30 +0200

    update acdordion in widget segment

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,14 +1,25 @@
-import react from "react";
+import react, { useState } from "react";
 
 const Accordion = ({ items }) => {
-	const renderedItems = items.map((item) => {
+	const [activeIndex, setActiveIndex] = useState(null);
+
+	const onTitleClick = (index) => {
+		setActiveIndex(index);
+	};
+
+	const renderedItems = items.map((item, index) => {
+		const active = index === activeIndex ? "active" : "";
+
 		return (
 			<react.Fragment key={item.title}>
-				<div className="title active">
+				<div
+					onClick={() => onTitleClick(index)}
+					className={`title ${active}`}
+				>
 					<i className="dropdown icon"></i>
 					{item.title}
 				</div>
-				<div className="content active">
+				<div className={`content ${active}`}>
 					<p>{item.content}</p>
 				</div>
 			</react.Fragment>
